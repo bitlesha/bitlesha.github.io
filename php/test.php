c6caa6c09112690b828c17a4041a3b22bf6262ac
bitlesha <98666013+bitlesha@users.noreply.github.com>
2022-04-06 22:52:49 +0300

    Delete php directory

--- php/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$name = $_POST['user_name'];
-$phone = $_POST['user_tel'];
-$message = $_POST['user_message'];
-
-echo "те пришла инфа <br>" . $name . "<br>" . $phone . "<br>" . $message ;
-?>
